d8bdd1677bcecfc0001ea97016c40c40c3bb8d1c
erivanstdio <erivanstdio@gmail.com>
2021-09-14 02:31:43 -0300

    removendo código obsoleto

--- src/services/api.js
+++ src/services/api.js
@@ -4,9 +4,7 @@ import md5 from "md5";
 const publicKey = '852d811eb90811a9038fcb8f50ba5558'
 const privateKey = '52a539eab722e3aec1e4ac975699d70eabc85802'
 const time = Number(new Date());
-const url = 'http://gateway.marvel.com/v1/public/characters?'
 
-const urlHelp = `${url}ts=${time}&apikey=${publicKey}&hash=${hash}`
 
 
 const hash = md5(time + privateKey + publicKey)
@@ -17,17 +15,6 @@ const api = axios.create({
         ts: time,
         apikey: publicKey,
         hash: hash,
-        orderBy: '-modified',
-    }
-})
-
-const busca = axios.create({
-    baseURL: 'http://gateway.marvel.com/v1/public',
-    params: {
-        ts: time,
-        apikey: publicKey,
-        hash: hash,
-        limit: 20,
         orderBy: '-modified',
     }
 })
